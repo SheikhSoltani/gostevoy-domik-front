8313873f894e9a894c0eb16d574fe95f497b85e7
Amir <amir53674268@gmail.com>
2024-02-22 23:28:21 +0600

    feature: metatags

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,6 @@ import Body from '@components/Body.vue'
 import Footer from '@components/Footer.vue'
 
 
-import { useHead } from '@vueuse/head';
-
-const title = "Гостевой домик";
-const description = 'Откройте горный опыт в "Gostevoy Domik Almatau" в Алматы! Горнолыжный курорт, активный отдых и уютные номера – идеальное сочетание для незабвенного отпуска. Бронируйте онлайн и погрузитесь в атмосферу "Гостевого домика"!';
-const og_description = 'Откройте горный опыт в "Gostevoy Domik Almatau" в Алматы! Горнолыжный курорт, активный отдых и уютные номера – идеальное сочетание для незабвенного отпуска. Бронируйте онлайн и погрузитесь в атмосферу "Гостевого домика"!';
-const keywords = "отель, апарт, отдых, горы, Алматы, гостиница, Алматау, кемпинг, сауна, номер, снять, мангал, гостевой домик";
-
-useHead({
-  title,
-  meta: [
-    {
-      name: "description",
-      content: description,
-    },
-    {
-      name: "keywords",
-      content: keywords,
-    },
-    {
-      property: 'og:description',
-      content: og_description,
-    },
-    {
-      httpEquiv: "Cache-Control",
-      content: "max-age=3600",
-    },
-    {
-      httpEquiv: "Expires",
-      content: "Sat, 12 Feb 2025 12:00:00 GMT",
-    }
-  ],
-});
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {createHead} from "@vueuse/head";
 
-const head=createHead()
 const app=createApp(App)
-app.use(head)
 app.mount('#app')
